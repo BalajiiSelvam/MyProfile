3736016d79f5127adb4a27714a57ceed9ed7f316
BalajiiSelvam <balajiselvam245@gmail.com>
2024-08-10 22:10:53 +0530

    Resumeadded

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,6 +4,8 @@ import Stats from '../../components/stats'
 import {FaDownload} from 'react-icons/fa';
 import CV from "../../assets/steve-Cv.pdf";
 import Skills from '../../components/skills';
+// import {resume} from '../../data';
+// import {ResumeItem} from '../../components/ResumeItem'
 import "./about.css";
 
 export const About = () => {
@@ -40,6 +42,30 @@ export const About = () => {
         </div>
       </section>
       <div className='separator'></div>
+      <section className='resume'>
+      <h3 className='section__subtitle subtitle__center'>Experience & Education</h3>
+      <div className='resume__container grid'>
+          <div className='resume__data'>
+            {resume.map((val)=>{
+              if(val.category === 'experience'){
+                return(
+                   <ResumeItem key={val.id} {...val}/>
+                );
+              }
+            })}
+          </div>
+          <div className='resume__data'>
+            {resume.map((val)=>{
+              if(val.category === 'education'){
+                return (
+                  <ResumeItem key={val.id} {...val}/>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </section>
+      
    </main>
   )
 
