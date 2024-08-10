7d8945b0cf7718592d8cc9e78b2a6c9f10af7d6d
BalajiiSelvam <balajiselvam245@gmail.com>
2024-08-10 17:30:43 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Navbar } from './components/Navbar'
 import { Home } from './pages/home/Home'
 import { About } from './pages/about/About'
 import { Contact } from './pages/contact/Contact'
-import {Portfolio } from './pages/portfolio/portfolio'
+import {Portfolio } from './pages/portfolio/Portfolio'
 
 function App() {
   return (
